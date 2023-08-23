0a3318936b49e5d538e8ffaf25a33c82d7bbf2da
Jair Murillo <brook_98@live.com>
2023-08-23 14:19:57 -0600

    modificaciones variadas

---
+++ formulario.js
@@ -0,0 +1,95 @@
+var formulario = document.forms["form"] //se puso el .forms y se elimino el #
+
+formulario.addEventListener('submit', function(e) {   //se añadio el evento en el submit y se colocó la funcion 
+
+  e.preventDefault();  //se coloco Default
+  
+  var name = formulario.elements['name'] ; //name opara mejor comprension
+  var age = formulario.elements['age'];  //age para mejor compresion
+  var nationality = formulario.elements['nationality'];  //nationality para mejor compresion
+
+  var nombre = name.value;  
+  var edad = age.value;
+
+  var i = nationality.selectedIndex;
+  var nacionalidad = nationality.options[i].value;
+  console.log(nombre, edad);
+  console.log(nacionalidad);
+
+  if (nombre.length === 0) {
+    name.classList.add("error")  //se puso el name
+  }
+  if (edad < 18 || edad > 120) {
+    age.classList.add("error")  //se puse el age
+  }
+
+if (nombre.length > 0  && (edad > 18  && edad < 120) ) {
+  agregarInvitado(nombre, edad, nacionalidad)
+  }
+}); 
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina"
+  }
+  else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana"
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana"
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana"
+  }
+
+var lista = document.getElementById("lista-de-invitados")
+
+var elementoLista = document.createElement("div")
+elementoLista.classList.add("elemento-lista")
+lista.appendChild(elementoLista)
+
+/* var spanNombre = document.createElement("span")    //se borra es bloque para que no se repita 
+var inputNombre = document.createElement("input")
+var espacio = document.createElement("br")
+spanNombre.textContent = "Nombre: "
+inputNombre.value = nombre 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio) */
+
+function crearElemento(descripcion, valor) {
+var spanNombre = document.createElement("span")
+var inputNombre = document.createElement("input")
+var espacio = document.createElement("br")
+spanNombre.textContent = descripcion + ": "
+inputNombre.value = valor 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+}
+
+crearElemento("Nombre", nombre)
+crearElemento("Edad", edad)
+crearElemento("Nacionalidad", nacionalidad)
+
+
+var botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+var corteLinea = document.createElement("br")
+elementoLista.appendChild(corteLinea)
+elementoLista.appendChild(botonBorrar);
+
+ botonBorrar.onclick = function() {
+// this.parentNode.style.display = 'none';
+botonBorrar.parentNode.remove()
+  }
+}
